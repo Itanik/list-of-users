b4491c4d6a205709a00e18a052512731fed26bc5
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-10-23 19:26:15 +0300

    Move to view binding.

--- app/src/main/java/me/itanik/listofusers/ui/user_details/UserDetailsFragment.kt
+++ app/src/main/java/me/itanik/listofusers/ui/user_details/UserDetailsFragment.kt
@@ -3,36 +3,51 @@ package me.itanik.listofusers.ui.user_details
 import android.os.Bundle
 import android.text.Html
 import android.text.Spanned
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import me.itanik.listofusers.R
+import me.itanik.listofusers.databinding.FragmentUserDetailsBinding
 
 class UserDetailsFragment : Fragment(R.layout.fragment_user_details) {
+    private var _binding: FragmentUserDetailsBinding? = null
+    private val binding: FragmentUserDetailsBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<TextView>(R.id.userName).text = "User"
-        view.findViewById<TextView>(R.id.userEmail).text = "user@email.com"
-        view.findViewById<TextView>(R.id.userPhone).text =
-            formatHtml(R.string.phone_html_ph, "8800553535")
-        view.findViewById<TextView>(R.id.userAge).text = formatHtml(R.string.age_html_ph, "33")
-        view.findViewById<TextView>(R.id.userAddress).text =
-            formatHtml(R.string.address_html_ph, "address")
-        view.findViewById<TextView>(R.id.userCompany).text =
-            formatHtml(R.string.company_html_ph, "Company Inc.")
-        view.findViewById<TextView>(R.id.userRegistered).text =
-            formatHtml(R.string.registered_html_ph, "23.10.2022")
-        view.findViewById<TextView>(R.id.userAbout).text = formatHtml(
-            R.string.about_html_ph,
-            "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
-        )
-        view.findViewById<ImageView>(R.id.userFavoriteFruit).setImageResource(R.drawable.ic_apple)
-        view.findViewById<ImageView>(R.id.userEyeColor).setImageResource(R.drawable.ic_green_circle)
+        with(binding) {
+            userName.text = "User"
+            userEmail.text = "user@email.com"
+            userPhone.text = formatHtml(R.string.phone_html_ph, "8800553535")
+            userAge.text = formatHtml(R.string.age_html_ph, "33")
+            userAddress.text = formatHtml(R.string.address_html_ph, "address")
+            userCompany.text = formatHtml(R.string.company_html_ph, "Company Inc.")
+            userRegistered.text = formatHtml(R.string.registered_html_ph, "23.10.2022")
+            userAbout.text = formatHtml(
+                R.string.about_html_ph,
+                "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+            )
+            userFavoriteFruit.setImageResource(R.drawable.ic_apple)
+            userEyeColor.setImageResource(R.drawable.ic_green_circle)
+        }
     }
 
     private fun formatHtml(res: Int, s: String): Spanned {
         val formattedString = getString(res, s)
         return Html.fromHtml(formattedString, Html.FROM_HTML_MODE_LEGACY)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/me/itanik/listofusers/ui/user_list/UserListFragment.kt
+++ app/src/main/java/me/itanik/listofusers/ui/user_list/UserListFragment.kt
@@ -1,20 +1,38 @@
 package me.itanik.listofusers.ui.user_list
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import me.itanik.listofusers.R
+import me.itanik.listofusers.databinding.FragmentUserListBinding
 
 class UserListFragment : Fragment(R.layout.fragment_user_list) {
+    private var _binding: FragmentUserListBinding? = null
+    private val binding: FragmentUserListBinding get() = _binding!!
+
     private val viewModel: UserListViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return _binding?.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel
-        view.findViewById<Button>(R.id.detailsButton).setOnClickListener {
+        binding.detailsButton.setOnClickListener {
             findNavController().navigate(R.id.action_userListFragment_to_userDetailsFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
